8322226e36ce7b5a5874e0df3708cd1fa51a0845
Silvana Tauhata Ynemine <silvana.ynemine.roda@outlook.com>
2023-12-28 16:41:59 -0300

    upload codigo fonte

---
+++ app/src/main/java/co/silvanaty/geradormega/MainActivity.kt
@@ -0,0 +1,84 @@
+package co.silvanaty.geradormega
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var prefs: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText: EditText = findViewById(R.id.edit_number)
+        val txtResult: TextView = findViewById(R.id.txt_result)
+        val btnGenerate: Button = findViewById(R.id.btn_generate)
+
+        prefs = getSharedPreferences("db", Context.MODE_PRIVATE)
+        val result = prefs.getString("result",null)
+
+        //if(result !=null){
+        //    txtResult.text = "Última aposta gerada: \n $result"
+        //}
+
+        //let ao invés de if
+        result?.let{
+            txtResult.text = "Última aposta gerada: \n $it"
+        }
+
+        btnGenerate.setOnClickListener {
+
+            val text = editText.text.toString()
+            numberGenerator(text, txtResult)
+        }
+    }
+
+    private fun numberGenerator(text: String, txtResult: TextView) {
+        if (text.isEmpty()) {
+            Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
+        }
+        val qtd = text.toInt()
+        if (qtd < 6 || qtd > 15) {
+            Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val numbers = mutableSetOf<Int>()
+        val random = Random()
+
+        while (true) {
+            val number = random.nextInt(60) // 0 a 59
+            numbers.add(number + 1)
+
+            if (numbers.size == qtd) {
+                break
+            }
+        }
+
+        val formattedNumbers = numbers.map { String.format("%02d", it) }
+        val ordered = formattedNumbers.toList().sorted()
+        val numbersGenerated = ordered.joinToString(" - ")
+        txtResult.text = "Aposta gerada: \n $numbersGenerated"
+
+        prefs.edit().apply{
+            putString("result", numbersGenerated)
+            apply()
+        }
+
+        //for (x in numbers){
+        //    Log.i("Teste","números que saiu: $x")
+        //}
+
+
+    }
+
+}
